95f3e881691d434baf333b9056c35290962b3661
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-08-12 22:14:52 -0400

    Cleaned up setup.py.

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-
 from distutils.core import setup
 
-setup(name='refmanage',
-      version='0.0.1',
-      author='Joshua Ryan Smith',
-      author_email='joshua.r.smith@gmail.com',
-      packages=['refmanage'],
-      url='https://github.com/jrsmith3/refmanage',
-      description='Manage a BibTeX database.',
-      install_requires=[],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      license='MIT',
-      zip_safe=False,
-      entry_points= {"console_scripts":
-            ["refmanage = refmanage.refmanage:main",]
-      },)
+setup(name = "refmanage",
+      version = "0.0.1",
+      author = "Joshua Ryan Smith",
+      author_email = "joshua.r.smith@gmail.com",
+      packages = ["refmanage"],
+      url = "https://github.com/jrsmith3/refmanage",
+      description = "Manage a BibTeX database.",
+      classifiers = ["Programming Language :: Python",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Development Status :: 3 - Alpha",
+                     "Intended Audience :: Science/Research",
+                     "Topic :: Text Processing",
+                     "Natural Language :: English",],
+      install_requires = ["pybtex"],)
