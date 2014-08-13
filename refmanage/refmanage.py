564bbc3eb17bd6a943f339b7677ccbbf816fd05e
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-08-12 22:49:51 -0400

    Method to return list of .bib files in specified directory. Sketched command-line functionality to combine all .bib files in a single file.

--- refmanage/refmanage.py
+++ refmanage/refmanage.py
@@ -4,7 +4,8 @@
 """
 refmanage - Manage a BibTeX database.
 """
-
+import os
+import argparse
 import pybtex.database as db
 from pybtex.database.input import bibtex
 from pybtex.database.output.bibtex import Writer
@@ -21,3 +22,53 @@ def merge_bib(*args):
             new_bib.add_entry(entry.key, entry)
 
     return new_bib
+
+def get_bib_filenames(fqpn):
+    """
+    Returns a list of fully-qualified paths of only bibTeX files.
+    """
+    filenames = [f for f in os.listdir(fqpn) if os.path.isfile(os.path.join(fqpn,f))]
+
+    bib_filenames = [f for f in filenames if f.endswith(".bib")]
+
+    return bib_filenames
+
+
+def main():
+    """
+    Command line interface.
+    """
+    parser = argparse.ArgumentParser(description = "Manage bibTeX files.")
+
+    parser.add_argument("target",
+        help = "File to contain combined bibTeX.")
+
+    args = parser.parse_args()
+
+    pwd = os.getcwd()
+    target_fqpn = os.path.join(pwd, args.target)
+    if os.path.exists(target_fqpn):
+        raise OSError("%s already exists." % target_fqpn)
+
+    bib_filenames = get_bib_filenames(pwd)
+    filenames_failed_imports = []
+    combined_bib = db.BibliographyData()
+
+    for filename in bib_filenames:
+        try:
+            parser = bibtex.Parser()
+            bib = parser.parse_file(filename)
+            del parser
+        except:
+            filenames_failed_imports.append(filename)
+
+        combined_bib = merge_bib(combined_bib, bib)
+
+    # Write combined_bib to the specified file.
+    w = Writer()
+    w.write_file(combined_bib, target_fqpn)
+
+
+    if len(filenames_failed_imports) > 0:
+        print "Could not parse the following files:"
+        print filenames_failed_imports
