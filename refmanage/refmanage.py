cf138e6cb9df2b231239b37958df793d9f6c3d1b
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-08-21 11:29:52 -0400

    Separated determination of target fully-qualified pathname from `import_target` method.

--- refmanage/refmanage.py
+++ refmanage/refmanage.py
@@ -95,16 +95,15 @@ def list_bibtex_at_sources(sources):
     return bib_filenames
 
 
-def import_target(target_pn, overwrite = False):
+def import_target(target_fqpn, overwrite = False):
     """
     Returns pybtex database of target bibTeX database.
 
-    If the path to `target_pn`, an exception will be raised. If the path to `target_pn` exists, but the file does not, the `overwrite` flag is moot and an empty pybtex database will be returned.
+    If the path to `target_fqpn`, an exception will be raised. If the path to `target_fqpn` exists, but the file does not, the `overwrite` flag is moot and an empty pybtex database will be returned.
 
-    :param str target_pn: Path to target bibTeX database. This path can be relative or absolute, but it must point to a file. The filename can have an arbitrary (or no) extension.
+    :param str target_fqpn: Fully-qualified pathname to target bibTeX database. This path must be absolute, but it must point to a file. The filename can have an arbitrary (or no) extension.
     :param bool overwrite: Switch to append source bibTeX to target or overwrite target with source bibTeX. Default = False.
     """
-    target_fqpn = os.path.abspath(target_pn)
     target_parent_dir = os.path.dirname(target_fqpn)
 
     if not os.path.isdir(target_parent_dir):
@@ -141,8 +140,12 @@ def cl_merge(cl_args):
 
     # Note the following merges everything without regards to duplicates or changing bibTeX keys to UIDs.
     sources_bib = merge_pybdb(source_bib_filenames_files.values())
-    target_bib = import_target(cl_args.target, cl_args.overwrite)
 
+    # Import the target according to the `overwrite` flag, but first get the target's fully-qualified path name.
+    target_fqpn = os.path.abspath(cl_args.target)
+    target_bib = import_target(target_fqpn, cl_args.overwrite)
+
+    # Combine the target and sources
     combined_bib = merge_pybdb(target_bib, sources_bib)
 
     # Write combined_bib to the specified file.
