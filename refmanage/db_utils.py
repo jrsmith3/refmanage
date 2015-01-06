3bc91d801e440bd04ea0758191b515af8f15aaa2
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-01-06 18:37:10 -0500

    Stub necessary functionality

--- refmanage/db_utils.py
+++ refmanage/db_utils.py
@@ -9,10 +9,24 @@ def cat_db(*args):
     """
     Concatenate multiple bib databases, return result
     """
-    new_bib = db.BibliographyData()
+    bib = db.BibliographyData()
 
     for arg in args:
         for entry in arg.entries.itervalues():
-            new_bib.add_entry(entry.key, entry)
+            bib.add_entry(entry.key, entry)
 
-    return new_bib
+    return bib
+
+
+def entries_missing_uris(bib):
+    """
+    Return keys of database entries missing a URI
+    """
+    pass
+
+
+def set_keys_to_uris(bib):
+    """
+    Return database with each entry key set to its URI
+    """
+    pass
