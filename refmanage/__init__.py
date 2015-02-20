640eb27d227747711da8a29785501b7b8465f4f4
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-02-20 15:31:06 -0500

    Stub python package

---
+++ refmanage/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+===============================
+Base Library (:mod:`refmanage`)
+===============================
+
+.. currentmodule:: refmanage
+"""
+
+__version__ = "0.0.2"
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import refmanage
+
+setup(name="refmanage",
+      version=refmanage.__version__,
+      author="Joshua Ryan Smith",
+      author_email="joshua.r.smith@gmail.com",
+      packages=["refmanage"],
+      url="https://github.com/jrsmith3/refmanage",
+      description="Manage a BibTeX database",
+      classifiers=["Programming Language :: Python",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Development Status :: 3 - Alpha",
+                   "Intended Audience :: Science/Research",
+                   "Topic :: Text Processing",
+                   "Natural Language :: English", ],
+      install_requires=["pybtex"], )
