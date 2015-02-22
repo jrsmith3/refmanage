a5ed56a22c4414a94c450740644fc53be1a6bd3f
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-02-21 19:31:27 -0500

    Stub tests for fs_utils

--- test/test_fs_utils.py
+++ test/test_fs_utils.py
@@ -1,2 +1,30 @@
 # -*- coding: utf-8 -*-
 import unittest
+from refmanage import fs_utils
+
+
+class MethodsInput(unittest.TestCase):
+    """
+    Tests methods which take input parameters
+
+    Tests include: passing invalid input, etc.
+    """
+    pass
+
+
+class MethodsReturnType(unittest.TestCase):
+    """
+    Tests methods' output types
+    """
+    def test_handle_files_args(self):
+        """
+        refmanage.fs_utils.handle_files_args should return a list
+        """
+        self.assertIsInstance(fs_utils.handle_files_args(""), list)
+
+
+class MethodsReturnValues(unittest.TestCase):
+    """
+    Tests values of methods against known values
+    """
+    pass
