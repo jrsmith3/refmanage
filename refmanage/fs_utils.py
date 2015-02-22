f8234b03546ee5ca5a52a82439e278e09ab76654
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-02-21 19:32:05 -0500

    Fix fs_utils.handle_files_args
    This method wasn't written very well initially. I fixed the logic and I
    also changed the input to accept an arbitrary number of strings. This
    `*paths_args` construction is much more explicit as to what the input
    needs to be.
    
    Closes #23.

--- refmanage/fs_utils.py
+++ refmanage/fs_utils.py
@@ -4,14 +4,17 @@ import glob
 import pathlib2 as pathlib
 
 
-def handle_files_args(paths_args):
+def handle_files_args(*paths_args):
     """
-    Handle files arguments from command line
+    Handle file(s) arguments from command line
 
-    This method takes a list of strings representing paths passed to the cli. It expands the path arguments and creates a list of pathlib.Path objects which unambiguously point to the files indicated by the command line arguments.
+    This method takes the string(s) which were passed to the cli which indicate the files on which to operate. It expands the path arguments and creates a list of `pathlib.Path` objects which unambiguously point to the files indicated by the cli arguments.
 
-    :param list paths_args: Paths to files.
+    :param str *paths_args: Paths to files.
+    :rtype: list
     """
+    paths = []
+
     for paths_arg in paths_args:
         # Handle paths implicitly rooted at user home dir
         paths_arg = os.path.expanduser(paths_arg)
@@ -20,6 +23,7 @@ def handle_files_args(paths_args):
         paths_arg = glob.glob(paths_arg)
 
         # Create list of pathlib.Path objects
-        paths = [pathlib.Path(path_arg) for path_arg in paths_arg]
+        paths.extend([pathlib.Path(path_arg) for path_arg in paths_arg])
 
     return paths
+
