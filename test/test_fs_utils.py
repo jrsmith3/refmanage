ccfa1ac94f335ec23fda610d0d2390d73d621818
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-02-24 16:29:40 -0500

    Test import_bib_files
    Closes #26.

--- test/test_fs_utils.py
+++ test/test_fs_utils.py
@@ -2,6 +2,7 @@
 import unittest
 import pathlib2 as pathlib
 from refmanage import fs_utils
+import pybtex
 
 
 class MethodsInput(unittest.TestCase):
@@ -36,34 +37,61 @@ class MethodsReturnType(unittest.TestCase):
         """
         path = pathlib.Path("test/controls/empty.bib")
         bibs = fs_utils.import_bib_files(path)
-        self.assertAll(bibs.keys(), pathlib.Path)
+        for key in bibs.keys():
+            self.assertIsInstance(key, pathlib.Path)
 
 
 class MethodsReturnValues(unittest.TestCase):
     """
     Tests values of methods against known values
     """
+    # Type check output of import_bib_files
     def test_import_bib_files_one_valid_entry_type(self):
         """
         refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing valid BibTeX
         """
-        pass
+        path = pathlib.Path("test/controls/one.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertIsInstance(bibs[path], pybtex.database.BibliographyData)
 
     def test_import_bib_files_two_valid_entry_type(self):
         """
         refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing valid BibTeX
         """
-        pass
+        path = pathlib.Path("test/controls/two.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertIsInstance(bibs[path], pybtex.database.BibliographyData)
 
     def test_import_bib_files_one_invalid_entry_type(self):
         """
         refmanage.fs_utils.import_bib_files should return a dict with a `None` value if the method argument points to a file containing invalid BibTeX
         """
-        pass
+        path = pathlib.Path("test/controls/invalid.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertIsNone(bibs[path], None)
 
     def test_import_bib_files_one_valid_one_invalid_entry_type(self):
         """
         refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing one valid and one invalid BibTeX entry
         """
-        pass
+        path = pathlib.Path("test/controls/one_valid_one_invalid.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertIsNone(bibs[path], None)
 
+
+    # Test number of entries in output of import_bib_files
+    def test_import_bib_files_one_valid_entry_number(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value with the same number of entries as the file.
+        """
+        path = pathlib.Path("test/controls/one.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertEqual(len(bibs[path].entries), 1)
+
+    def test_import_bib_files_two_valid_entry_number(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing valid BibTeX
+        """
+        path = pathlib.Path("test/controls/two.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertEqual(len(bibs[path].entries), 2)
