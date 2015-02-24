9e3108870586c54aaeda50ae935f7f8e8b87da35
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-02-24 10:46:15 -0500

    Stub out tests for `import_bib_files` method

--- test/test_fs_utils.py
+++ test/test_fs_utils.py
@@ -30,9 +30,40 @@ class MethodsReturnType(unittest.TestCase):
         path = pathlib.Path("test/controls/empty.bib")
         self.assertIsInstance(fs_utils.import_bib_files(path), dict)
 
+    def test_import_bib_files_key(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with key `pathlib.Path`
+        """
+        path = pathlib.Path("test/controls/empty.bib")
+        bibs = fs_utils.import_bib_files(path)
+        self.assertAll(bibs.keys(), pathlib.Path)
+
 
 class MethodsReturnValues(unittest.TestCase):
     """
     Tests values of methods against known values
     """
-    pass
+    def test_import_bib_files_one_valid_entry_type(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing valid BibTeX
+        """
+        pass
+
+    def test_import_bib_files_two_valid_entry_type(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing valid BibTeX
+        """
+        pass
+
+    def test_import_bib_files_one_invalid_entry_type(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with a `None` value if the method argument points to a file containing invalid BibTeX
+        """
+        pass
+
+    def test_import_bib_files_one_valid_one_invalid_entry_type(self):
+        """
+        refmanage.fs_utils.import_bib_files should return a dict with a `pybtex.database.bibtex` value if the method argument points to a file containing one valid and one invalid BibTeX entry
+        """
+        pass
+
