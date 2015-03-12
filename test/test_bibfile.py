b15d28faa3d9bd78f32a9d4bfca41ac20caa6da5
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-03-12 07:51:09 -0400

    Stub tests for `BibFile`

---
+++ test/test_bibfile.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import unittest
+import pathlib2 as pathlib
+from refmanage import fs_utils
+from pybtex.database import BibliographyData
+from pybtex.exceptions import PybtexError
+
+class MethodsInput(unittest.TestCase):
+    """
+    Tests methods which take input parameters
+
+    Tests include: passing invalid input, etc.
+    """
+    pass
+
+
+class MethodsReturnType(Base):
+    """
+    Tests methods' output types
+    """
+    pass
+
+
+class MethodsReturnValues(Base):
+    """
+    Tests values of methods against known values
+    """
+    pass
