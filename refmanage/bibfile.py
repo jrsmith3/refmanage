34ab3eb7f83f7c059313309e64b528e81bb683cd
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-03-11 23:29:09 -0400

    Move functionality from fs_utils.py to bibfile.py
    This operation was a simple copy/paste -- I made no attempt to refactor
    the methods to be methods of the `BibFile` class.

--- refmanage/bibfile.py
+++ refmanage/bibfile.py
@@ -2,5 +2,75 @@
 
 class BibFile(object):
     """
+    :param pathlib.Path path: Path to file possibly containing BibTeX data.
     """
-    pass
+    def __init__(self, path):
+        self.path = path
+
+
+def parse_bib_file(path):
+    """
+    Parse BibTeX file located at `path`
+
+    This method attempts to parse the BibTeX file located at `path`. If the file is parseable, a `pybtex.database.BibliographyData` object is returned, containing the bibliography data contained in the file. If the file is unparseable, the exception raised by the parser is returned.
+
+    :param pathlib.Path path: Path to file possibly containing BibTeX data.
+    """
+    parser = bibtex.Parser()
+    try:
+        bib = parser.parse_file(str(path.resolve()))
+    except PybtexError, e:
+        bib = e
+
+    return bib
+
+
+def gen_terse_msg(path):
+    """
+    STDOUT message corresponding to `path`
+
+    This method generates and returns the message to be returned to STDOUT which corresponds to the `path` argument to this method.
+
+    :param pathlib.Path path: Path to file possibly containing BibTeX data.
+    :rtype: str
+    """
+    terse_msg = str(path.resolve())
+    return terse_msg
+
+
+def gen_verbose_msg(bib):
+    """
+    STDOUT message corresponding to `path` when --verbose set
+
+    This method generates and returns the message to be returned to STDOUT which corresponds to the `path` argument to this method in the event the "--verbose" flag was passed on the command-line. This method can accept an arguent of two different types: a `pybtex.database.BibliographyData` or `pybtex.exceptions.PybtexError`. If `bib` is of type `BibliographyData`, this method will return an empty string. If `bib` is of type `PybtexError`, this method will gather data from the exception into a string which is returned.
+
+    :param bib: Output of `parse_bib_file`; can be either a `pybtex.database.BibliographyData` or `pybtex.exceptions.PybtexError`.
+    :rtype: str
+    """
+    msg = ""
+    if isinstance(bib, TokenRequired):
+        msg += bib.error_type + ": "
+        msg += bib.message + "\n"
+        msg += str(bib.lineno) + " "
+        msg += bib.get_context()
+    elif isinstance(bib, PybtexError):
+        msg += bib.message
+
+    return msg
+
+def gen_bib_dict_test_msg(bib_dict, verbose=False):
+    """
+    Generate appropriate STDOUT message given bib_dict
+
+    This method creates the string to be printed to STDOUT for a single bib_dict. It generates either a terse or verbose message based on the state of the `verbose` argument.
+
+    :param dict bib_dict: Dictionary containing bib file information.
+    :param bool verbose: Directive to construct verbose/terse STDOUT string.
+    :rtype: str
+    """
+    msg = bib_dict["terse_msg"]
+    if verbose:
+        msg += "\n" + bib_dict["verbose_msg"]
+
+    return msg
+
--- refmanage/fs_utils.py
+++ refmanage/fs_utils.py
@@ -38,83 +38,6 @@ def handle_files_args(*paths_args):
     return paths
 
 
-def construct_bib_dict(path):
-    """
-    Create dict containing data for use in refmanage
-
-    This method creates dicts containing information for use in refmanage corresponding to the file indicated by `path`. Each dictionary contains the following data:
-
-    * path: The `pathlib.Path` object passed as the argument to this method.
-    * bib: BibTeX data, stored as a `pybtex.database.BibliographyData` object, parsed from the file indicated by the `path` key. If the file was unparseable, the exception raised during parsing is stored in this variable.
-    * terse_msg: Message corresponding to the file to be printed to STDOUT when the "--verbose" flag is absent from the command line.
-    * verbose_msg: Message corresponding to the file to be printed to STDOUT when the "--verbose" flag is passed on the command line.
-
-    :param pathlib.Path path: Path to file possibly containing BibTeX data.
-    :rtype: dict
-    """
-    bib = parse_bib_file(path)
-    terse_msg = gen_terse_msg(path)
-    verbose_msg = gen_verbose_msg(bib)
-
-    bib_dict = {"path": path,
-        "bib": bib,
-        "terse_msg": terse_msg,
-        "verbose_msg": verbose_msg, }
-
-    return bib_dict
-
-
-def parse_bib_file(path):
-    """
-    Parse BibTeX file located at `path`
-
-    This method attempts to parse the BibTeX file located at `path`. If the file is parseable, a `pybtex.database.BibliographyData` object is returned, containing the bibliography data contained in the file. If the file is unparseable, the exception raised by the parser is returned.
-
-    :param pathlib.Path path: Path to file possibly containing BibTeX data.
-    """
-    parser = bibtex.Parser()
-    try:
-        bib = parser.parse_file(str(path.resolve()))
-    except PybtexError, e:
-        bib = e
-
-    return bib
-
-
-def gen_terse_msg(path):
-    """
-    STDOUT message corresponding to `path`
-
-    This method generates and returns the message to be returned to STDOUT which corresponds to the `path` argument to this method.
-
-    :param pathlib.Path path: Path to file possibly containing BibTeX data.
-    :rtype: str
-    """
-    terse_msg = str(path.resolve())
-    return terse_msg
-
-
-def gen_verbose_msg(bib):
-    """
-    STDOUT message corresponding to `path` when --verbose set
-
-    This method generates and returns the message to be returned to STDOUT which corresponds to the `path` argument to this method in the event the "--verbose" flag was passed on the command-line. This method can accept an arguent of two different types: a `pybtex.database.BibliographyData` or `pybtex.exceptions.PybtexError`. If `bib` is of type `BibliographyData`, this method will return an empty string. If `bib` is of type `PybtexError`, this method will gather data from the exception into a string which is returned.
-
-    :param bib: Output of `parse_bib_file`; can be either a `pybtex.database.BibliographyData` or `pybtex.exceptions.PybtexError`.
-    :rtype: str
-    """
-    msg = ""
-    if isinstance(bib, TokenRequired):
-        msg += bib.error_type + ": "
-        msg += bib.message + "\n"
-        msg += str(bib.lineno) + " "
-        msg += bib.get_context()
-    elif isinstance(bib, PybtexError):
-        msg += bib.message
-
-    return msg
-
-
 def construct_bibfile_data(*paths):
     """
     List of data corresponding to individual bib files
@@ -155,21 +78,4 @@ def gen_stdout_test_msg(bibfile_data, verbose=False):
     """
     msg_list = [gen_bib_dict_test_msg(bib_dict, verbose) for bib_dict in bibfile_data]
     msg = "\n\n".join(msg_list)
-    return msg
-
-
-def gen_bib_dict_test_msg(bib_dict, verbose=False):
-    """
-    Generate appropriate STDOUT message given bib_dict
-
-    This method creates the string to be printed to STDOUT for a single bib_dict. It generates either a terse or verbose message based on the state of the `verbose` argument.
-
-    :param dict bib_dict: Dictionary containing bib file information.
-    :param bool verbose: Directive to construct verbose/terse STDOUT string.
-    :rtype: str
-    """
-    msg = bib_dict["terse_msg"]
-    if verbose:
-        msg += "\n" + bib_dict["verbose_msg"]
-
     return msg
